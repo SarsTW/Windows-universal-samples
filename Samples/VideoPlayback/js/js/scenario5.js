80735368a4d12e5affef28c40cc44d8a8f3ffdf6
Sars Chang <github@sars.tw>
2015-11-17 12:09:08 +0800

    [VideoPlayback] Modify scenario 5 to test background audio playback.

--- Samples/VideoPlayback/js/js/scenario5.js
+++ Samples/VideoPlayback/js/js/scenario5.js
@@ -2,24 +2,33 @@
     "use strict";
     var Media = Windows.Media;
     var playbackItem;
+    var systemMediaControls = null;
 
-    var mainVideo;
+    var mainAudio;
     var page = WinJS.UI.Pages.define("/html/scenario5.html", {
         ready: function (element, options) {
             document.getElementById("btn1").addEventListener("click", btn1Click, false);
             document.getElementById("btn2").addEventListener("click", btn2Click, false);
             document.getElementById("btn3").addEventListener("click", btn3Click, false);
 
+            systemMediaControls = Windows.Media.SystemMediaTransportControls.getForCurrentView();
+            systemMediaControls.addEventListener("buttonpressed", mediaButtonPressed, false);
+            systemMediaControls.isPlayEnabled = true;
+            systemMediaControls.isPauseEnabled = true;
+            systemMediaControls.playbackStatus = Windows.Media.MediaPlaybackStatus.closed;
 
-            mainVideo = document.getElementById("mainVideo");
+            mainAudio = document.getElementById("mainAudio");
+            mainAudio.addEventListener("playing", audioPlaying, false);
+            mainAudio.addEventListener("pause", audioPaused, false);
+
             var playlist = new Media.Playback.MediaPlaybackList();
-            var source1 = Media.Core.MediaSource.createFromUri(new Windows.Foundation.Uri("https://mediaplatstorage1.blob.core.windows.net/windows-universal-samples-media/elephantsdream-clip-h264_sd-aac_eng-aac_spa-aac_eng_commentary-srt_eng-srt_por-srt_swe.mkv"));
 
+            var source1 = Media.Core.MediaSource.createFromUri(new Windows.Foundation.Uri("http://demos.w3avenue.com/html5-unleashed-tips-tricks-and-techniques/demo-audio.mp3"));
             playbackItem = new Media.Playback.MediaPlaybackItem(source1);
             playlist.items.append(playbackItem);
 
 
-            mainVideo.src = URL.createObjectURL(playlist, { oneTimeOnly: true });
+            mainAudio.src = URL.createObjectURL(playlist, { oneTimeOnly: true });
 
 
         }
@@ -39,5 +48,43 @@
         playbackItem.audioTracks.selectedIndex = 2;
     }
 
+    function audioPlaying() {
+        systemMediaControls.playbackStatus = Windows.Media.MediaPlaybackStatus.playing;
+    }
+    function audioPaused() {
+        systemMediaControls.playbackStatus = Windows.Media.MediaPlaybackStatus.paused;
+    }
+
+    function mediaButtonPressed(e) {
+        switch (e.button) {
+            case Windows.Media.SystemMediaTransportControlsButton.play:
+                mainAudio.play();
+                break;
+            case Windows.Media.SystemMediaTransportControlsButton.pause:
+                mainAudio.pause();
+                break;
+            default:
+                break;
+        }
+    }
+
+    // The event handler for the play/pause button
+    function playpausetoggle() {
+        if (mediaControls.isPlaying === true) {
+            mainAudio.pause();
+        } else {
+            mainAudio.play();
+        }
+    }
+
+    // The event handler for the pause button
+    function pausebutton() {
+        mainAudio.pause();
+    }
+
+    // The event handler for the play button
+    function playbutton() {
+        mainAudio.play();
+    }
 
 })();
